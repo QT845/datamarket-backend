ea7af46ab06d84bab6f146800c19cb15402e1aee
QT845 <thinh200584@gmail.com>
2025-12-01 16:56:32 +0700

    Initialize Spring Boot project

---
+++ datamarket/src/main/java/com/datamarket/backend/DatamarketApplication.java
@@ -0,0 +1,13 @@
+package com.datamarket.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatamarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatamarketApplication.class, args);
+    }
+
+}
---
+++ datamarket/src/test/java/com/datamarket/backend/DatamarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datamarket.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatamarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
