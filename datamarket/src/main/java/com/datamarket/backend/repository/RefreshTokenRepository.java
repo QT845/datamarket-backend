21ed331a3dc46c234b2718022f605174331b2249
QT845 <thinh200584@gmail.com>
2025-12-10 01:29:44 +0700

    Implement global exception handling system

--- datamarket/src/main/java/com/datamarket/backend/repository/RefreshTokenRepository.java
+++ datamarket/src/main/java/com/datamarket/backend/repository/RefreshTokenRepository.java
@@ -3,13 +3,17 @@ package com.datamarket.backend.repository;
 import com.datamarket.backend.entity.RefreshToken;
 import com.datamarket.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-    Optional<RefreshToken> findByRefreshToken(String refreshToken);
-    List<RefreshToken> findAllByUserAndRevokedFalse(User user);
-    void deleteByExpiredAtBefore(LocalDateTime now);
+    Optional<RefreshToken> findByRefreshToken(String token);
+    void deleteByUser(User user);
+    void deleteByExpiredAtBefore(LocalDateTime dateTime);
+    void deleteByRefreshToken(String token);
+
 }
